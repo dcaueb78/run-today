1c269a3c88f104c99d57bf5e38c40d46bcf44a6a
Cauê Kotarski <caue1017@hotmail.com>
2020-06-12 21:40:19 -0300

    feat: create form inputs

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Image } from 'react-native';
-
-import { Container, Title } from './styles';
+import { Input, Button } from '../../components/FormInputs';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title> Entre na sua conta </Title>
+
+      <Input />
+      <Input />
+
+      <Button
+        onPress={() => {
+          console.log('irra');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -4,10 +4,12 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
+  margin: 20px 0;
 `;
