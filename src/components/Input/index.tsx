1bcecca547711531ca50e25e4dea5f0c2ea06173
Cauê Kotarski <caue1017@hotmail.com>
2020-06-12 22:11:25 -0300

    feat: create Input Icons

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TextInputProps } from 'react-native';
-import { Container, TextInput } from './styles';
+
+import { Container, TextInput, Icon } from './styles';
 
 interface InputProps extends TextInputProps {
   name: string;
@@ -9,6 +10,7 @@ interface InputProps extends TextInputProps {
 
 const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
   <Container>
+    <Icon name={icon} size={20} color="#666360" />
     <TextInput
       keyboardAppearance="dark"
       placeholderTextColor="#666360"
